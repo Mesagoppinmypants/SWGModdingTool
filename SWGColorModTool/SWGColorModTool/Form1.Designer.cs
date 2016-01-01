e2ab59b76487c6e90f5c83f0823d7fd37368f9ff
Iosnowore <pressinger.gene66@gmail.com>
2015-12-31 23:34:49 -0500

    Initial Code Commit

---
+++ SWGColorModTool/SWGColorModTool/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SWGColorModTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.FirstRowTimer = new System.Windows.Forms.Timer(this.components);
+            this.RebornDirTextBox = new System.Windows.Forms.TextBox();
+            this.TREDirTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EditDirectoriesButton = new System.Windows.Forms.Button();
+            this.CloseProgramsCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(197, 226);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // FirstRowTimer
+            // 
+            this.FirstRowTimer.Interval = 1000;
+            this.FirstRowTimer.Tick += new System.EventHandler(this.FirstRowTimer_Tick);
+            // 
+            // RebornDirTextBox
+            // 
+            this.RebornDirTextBox.Location = new System.Drawing.Point(12, 178);
+            this.RebornDirTextBox.Name = "RebornDirTextBox";
+            this.RebornDirTextBox.ReadOnly = true;
+            this.RebornDirTextBox.Size = new System.Drawing.Size(260, 20);
+            this.RebornDirTextBox.TabIndex = 1;
+            // 
+            // TREDirTextBox
+            // 
+            this.TREDirTextBox.Location = new System.Drawing.Point(12, 117);
+            this.TREDirTextBox.Name = "TREDirTextBox";
+            this.TREDirTextBox.ReadOnly = true;
+            this.TREDirTextBox.Size = new System.Drawing.Size(260, 20);
+            this.TREDirTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TRE Explorer Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SWG Reborn Directory";
+            // 
+            // EditDirectoriesButton
+            // 
+            this.EditDirectoriesButton.Location = new System.Drawing.Point(12, 215);
+            this.EditDirectoriesButton.Name = "EditDirectoriesButton";
+            this.EditDirectoriesButton.Size = new System.Drawing.Size(75, 34);
+            this.EditDirectoriesButton.TabIndex = 5;
+            this.EditDirectoriesButton.Text = "Edit Current Directories";
+            this.EditDirectoriesButton.UseVisualStyleBackColor = true;
+            this.EditDirectoriesButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CloseProgramsCheckBox
+            // 
+            this.CloseProgramsCheckBox.AutoSize = true;
+            this.CloseProgramsCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.CloseProgramsCheckBox.Name = "CloseProgramsCheckBox";
+            this.CloseProgramsCheckBox.Size = new System.Drawing.Size(166, 17);
+            this.CloseProgramsCheckBox.TabIndex = 6;
+            this.CloseProgramsCheckBox.Text = "Close Programs When Closed";
+            this.CloseProgramsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CloseProgramsCheckBox);
+            this.Controls.Add(this.EditDirectoriesButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TREDirTextBox);
+            this.Controls.Add(this.RebornDirTextBox);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SWG Color Mod Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer FirstRowTimer;
+        private System.Windows.Forms.TextBox RebornDirTextBox;
+        private System.Windows.Forms.TextBox TREDirTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button EditDirectoriesButton;
+        private System.Windows.Forms.CheckBox CloseProgramsCheckBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SWGColorModTool/SWGColorModTool/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWGColorModTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Properties.Settings.Default.SWGRebornDir) && !Directory.Exists(Properties.Settings.Default.TREDir))
+            {
+                DialogResult setupdirs = MessageBox.Show("ERROR: Both your TRE Explorer directory and SWG Reborn directory are blank, would you like to configure those?", "Configure Directories", MessageBoxButtons.YesNo);
+
+                if (setupdirs == DialogResult.Yes)
+                {
+                    ConfigRebornDir();
+                    ConfigTREDir();
+                }
+            }
+            else if (!Directory.Exists(Properties.Settings.Default.SWGRebornDir))
+            {
+                ConfigRebornDir();
+            }
+            else if (!Directory.Exists(Properties.Settings.Default.TREDir))
+            {
+                ConfigTREDir();
+            }
+            if (!Directory.Exists(RebornDirTextBox.Text + "\\palette"))
+            {
+                Directory.CreateDirectory(RebornDirTextBox.Text + "\\palette");
+            }
+
+            // TODO: Check if TRE Explorer is already running, if so, close all running ones.
+
+            Process.Start(RebornDirTextBox.Text + "\\sku0_client.toc");
+            Thread.Sleep(8000); // Wait for TRE Explorer to load
+            GoToPalette();
+
+            //FirstRowTimer.Enabled = true;
+            //FirstRowTimer.Start();
+        }
+
+        private void LoadNewLightsaberPal()
+        {
+
+        }
+
+        private void ConfigTREDir()
+        {
+            FolderBrowserDialog browseTREDir = new FolderBrowserDialog();
+            browseTREDir = new FolderBrowserDialog();
+            browseTREDir.Description = "Please select your TRE Explorer directory.";
+            browseTREDir.ShowDialog();
+            if (File.Exists(browseTREDir.SelectedPath + "\\TRE Explorer.exe"))
+            {
+                Properties.Settings.Default.TREDir = browseTREDir.SelectedPath;
+                Properties.Settings.Default.Save();
+                TREDirTextBox.Text = Properties.Settings.Default.TREDir;
+                MessageBox.Show("You have successfully configured your TRE Explorer directory path.", "Success");
+            }
+
+            if (!File.Exists(browseTREDir.SelectedPath + "\\Tre Explorer.exe"))
+            {
+                //MessageBox.Show("ERROR: The directory that you have chosen is an invalid TRE Explorer directory.", "Error");
+            }
+        }
+
+        private void ConfigRebornDir()
+        {
+            FolderBrowserDialog browseRebornDir = new FolderBrowserDialog();
+            browseRebornDir.Description = "Please select your SWG Reborn game directory.";
+            browseRebornDir.ShowDialog();
+            if (File.Exists(browseRebornDir.SelectedPath + "\\SwgClient_r.exe"))
+            {
+                Properties.Settings.Default.SWGRebornDir = browseRebornDir.SelectedPath;
+                Properties.Settings.Default.Save();
+                RebornDirTextBox.Text = Properties.Settings.Default.SWGRebornDir;
+                MessageBox.Show("You have successfully configured your SWG Reborn directory path.", "Success");
+                browseRebornDir.Dispose();
+            }
+
+            if (!File.Exists(browseRebornDir.SelectedPath + "\\SwgClient_r.exe"))
+            {
+                //MessageBox.Show("ERROR: The directory that you have chosen is an invalid SWG Reborn game directory.", "Error");
+            }
+            
+        }
+
+        private void GoToPalette()
+        {
+            Process treexplorer = Process.GetProcessesByName("TRE Explorer").FirstOrDefault();
+            if (treexplorer != null)
+            {
+                IntPtr h = treexplorer.MainWindowHandle;
+                SetForegroundWindow(h);
+                //SendKeys.SendWait("^{f}");
+                //SendKeys.SendWait("wp_lightsaber.pal");
+                //SendKeys.SendWait("{ENTER}");
+                //SendKeys.SendWait("{ESC}");
+                //SetForegroundWindow(h);
+                //SendKeys.SendWait("{DOWN}");
+                // TODO: Implement a way to select the option wanted.
+                //SendKeys.SendWait("DIR {ENTER}"); //Selects color and closes program.
+            }
+        }
+
+        private void FirstRowTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sorry, this function isn't implemented yet.", "Function Not Implemented");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CloseProgramsCheckBox.Checked = Properties.Settings.Default.ClosePrograms;
+            SoundPlayer illuminati = new SoundPlayer(Properties.Resources.illuminati);
+            illuminati.PlayLooping();
+            RebornDirTextBox.Text = Properties.Settings.Default.SWGRebornDir;
+            TREDirTextBox.Text = Properties.Settings.Default.TREDir;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.ClosePrograms = CloseProgramsCheckBox.Checked;
+            Properties.Settings.Default.Save();
+            if (CloseProgramsCheckBox.Checked == true)
+            {
+                Process treexplorer = Process.GetProcessesByName("TRE Explorer").FirstOrDefault();
+                treexplorer.Kill();
+            }
+        }
+    }
+}
